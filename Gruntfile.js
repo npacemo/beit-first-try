eb9237c770dc23bf8e6ce2bbd88f50d56c917421
Vladimir Tsvetkov <npacemo@gmail.com>
2014-05-19 21:51:43 +0300

    Add HTML layout, sidebar content, and initial LESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,6 +350,7 @@ module.exports = function (grunt) {
     less: {
       default: {
         options: {
+          paths: ["<%= yeoman.app %>/styles"],
           compress: false,
         },
         files: {
