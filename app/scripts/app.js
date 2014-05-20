d31fcb90a1b25da94bf044d4f792e2c35c6fbd26
Vladimir Tsvetkov <npacemo@gmail.com>
2014-05-20 14:07:05 +0300

    Add sidebar menu interaction

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'ngTouch'
   ])
   .config(function ($routeProvider) {
     $routeProvider
---
+++ app/scripts/controllers/root.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('firstTryApp')
+  .controller('RootCtrl', function ($scope) {
+    $scope.menuSidebarShown = false;
+
+    $scope.showSidebarMenu = function(visible) {
+      $scope.menuSidebarShown = visible;
+    };
+
+    $scope.hideSidebarMenu = function() {
+      $scope.menuSidebarShown = false;
+    };
+
+    $scope.$watch("menuSidebarShown", function() {
+      $scope.updateContainerWrapperTransform();
+    });
+
+    $scope.updateContainerWrapperTransform = function() {
+      var containerWrapper = $(".container-wrapper");
+      if ($scope.menuSidebarShown) {
+        containerWrapper.css("transform", "translate3d(80%, 0, 0)");
+      } else {
+        containerWrapper.css("transform", "");
+      }
+    };
+  });
