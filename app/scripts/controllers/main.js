41bfb287813ff0d1b17a8e2bc53b982ac348202b
Vladimir Tsvetkov <npacemo@gmail.com>
2014-05-21 00:15:34 +0300

    Demo application code complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,27 @@
 'use strict';
 
 angular.module('firstTryApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .factory('photos', function() {
+    var photos = [
+      { src: '../images/photos/DSC_9244.jpg', tags: ["Granpa", "Granma", "Krissy", "Annie", "Vladi"]},
+      { src: '../images/photos/DSC_9245.jpg', tags: ["Granpa", "Granma", "Krissy", "Annie", "Vladi"]},
+      { src: '../images/photos/DSC_9248.jpg', tags: ["Krissy", "Annie", "Vladi", "Victor", "Volen", "Raya"]},
+      { src: '../images/photos/DSC_9249.jpg', tags: ["Annie", "Alek", "Raya"]},
+      { src: '../images/photos/DSC_9250.jpg', tags: ["Annie", "Alek", "Dayanka"]},
+      { src: '../images/photos/DSC_9251.jpg', tags: ["Annie", "Kalo", "Dayanka"]},
+      { src: '../images/photos/DSC_9252.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9253.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9254.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9255.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9256.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9257.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9258.jpg', tags: ["Titi", "Dayanka"]},
+      { src: '../images/photos/DSC_9260.jpg', tags: ["Nesi", "Vladi"]},
+      { src: '../images/photos/DSC_9261.jpg', tags: ["Granma", "Annie", "Vladi"]},
+      { src: '../images/photos/DSC_9262.jpg', tags: ["Annie", "Vladi", "Volen"]}
     ];
+    return photos;
+  })
+  .controller('MainCtrl', function ($scope, photos) {
+    $scope.photos = photos;
   });
